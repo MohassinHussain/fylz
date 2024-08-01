59814104b6077def62c5e271982899a734e270e2
unknown <mohassinhussain22@gmail.com>
2024-08-01 16:36:36 +0530

    edited UI

--- archivenvo/src/Components/LandingPage.jsx
+++ archivenvo/src/Components/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
+// import PreviousButton from "./PreviousButton";
+import { BiSolidSkipPreviousCircle } from "react-icons/bi";
 
 export default function LandingPage() {
   //activators
@@ -19,12 +21,15 @@ export default function LandingPage() {
 
   //receive
   const [receiverCode, setReceiverCode] = useState("");
+//cancel request 
+  const cancelTokenSource = useRef(null);
+
 
   //upload clicked
   const submitFilesCode = async (e) => {
     e.preventDefault();
     if (code === "" || code === " ") {
-      setAlerter("Enter code First..")
+      setAlerter("Enter code First..");
     } else {
       // console.log(code, selectedFile);
 
@@ -34,6 +39,9 @@ export default function LandingPage() {
       formData.append("code", code);
       formData.append("file", selectedFile);
 
+      cancelTokenSource.current = axios.CancelToken.source();
+
+
       try {
         const result = await axios.post(
           "https://archivenvo.onrender.com/file-upload",
@@ -42,6 +50,7 @@ export default function LandingPage() {
             headers: {
               "Content-Type": "multipart/form-data",
             },
+            cancelToken: cancelTokenSource.current.token,
           }
         );
         //
@@ -51,10 +60,22 @@ export default function LandingPage() {
         setSelectedFile("");
         setUploadFilesClicked(false);
       } catch (error) {
-        console.log("ERROR NEAR UPLOADING", error);
+        if (axios.isCancel(error)) {
+          console.log("Request canceled:", error.message);
+        } else {
+          console.log("ERROR NEAR UPLOADING", error);
+        }
+        setUploadFilesClicked(false);
       }
     }
   };
+
+  function cancelled() {
+    setUploadFilesClicked(false)
+    if (cancelTokenSource.current) {
+      cancelTokenSource.current.cancel("Operation canceled by the user.");
+    }
+  }
 
   const [received, setReceived] = useState(null);
   const [url, setUrl] = useState("");
@@ -63,28 +84,34 @@ export default function LandingPage() {
     e.preventDefault();
     // console.log(receiverCode);
     setSubmitCodeClicked(true);
+    cancelTokenSource.current = axios.CancelToken.source();
+
     try {
-      const response = await axios.post(
-        "https://archivenvo.onrender.com/file-get",
-        {
-          receiverCode,
-        }
-      );
+      const response = await axios.post("https://archivenvo.onrender.com/file-get", {
+        receiverCode,
+      }, {            cancelToken: cancelTokenSource.current.token,
+      });
       const fileName = response.data.data?.fileName;
 
       if (fileName) {
         setUrl(`https://archivenvo.onrender.com/my-files/${fileName}`);
         setReceived(response.data.data);
-
+        
         setShowDownloadButton(true);
       } else {
         console.error("No fileName in response data");
-        setAlerter("No file exist with the code/ please enter precisely");
+          setAlerter("No file exist with the code/ please enter precisely");
+        
       }
       setReceiverCode("");
       setSubmitCodeClicked(false);
     } catch (error) {
-      console.log("ERROR WHILE SEMDING RECIVE CODE", error);
+      if (axios.isCancel(error)) {
+        console.log("Request canceled:", error.message);
+      } else {
+        console.log("ERROR NEAR UPLOADING", error);
+      }
+      setUploadFilesClicked(false);
     }
   };
 
@@ -126,17 +153,30 @@ export default function LandingPage() {
       console.error("URL or received data is not set");
     }
   }
+  const [preClicked, setPreClicked] = useState(true);
+  function previousClicked() {
+    // setPreClicked(true);
+    setUploadClicked(false);
+    setReceiveClicked(false);
+  }
 
   return (
     <div className="grid mt-16 mx-10 md:mx-48 ">
       <div className="bg-gradient-to-b from-slate-400 to-blue-200 font-bold rounded-t p-4">
-        <h1 className="text-4xl text-center">Welcome to ArchivEnvo!</h1>
+        <h1 className="text-4xl text-center font-serif">
+          Welcome to ArchivEnvo!
+        </h1>
       </div>
 
-      <div className="bg-gradient-to-t md:items-center md:flex md:flex-col rounded-b p-10 grid">
+      <div className="bg-gradient-to-b h-60 from-slate-300 to-b md:items-center md:flex md:flex-col rounded-b p-10 grid">
         {/* //Dialog box */}
         <div className="text-red-400 text-2xl font-bold">{alerter}</div>
-
+        {(uploadClicked || receiveClicked) && preClicked && (
+          <BiSolidSkipPreviousCircle
+            className="text-cyan-900 w-8 h-8 ml-64 md:ml-96"
+            onClick={previousClicked}
+          />
+        )}
         {/* <button className="previous bg-red-400 m-1 w-4 h-4 rounded-full" /> */}
         {uploadClicked ? (
           <form action="">
@@ -160,6 +200,13 @@ export default function LandingPage() {
               className="mt-5 bg-red-200 rounded ml-3  p-2  hover:bg-purple-300 hover:font-bold"
             >
               {uploadFilesClicked ? "Uploading..." : "Upload files"}
+            </button>
+            <button
+              // type="submit"
+              onClick={cancelled}
+              className="mt-5 bg-red-400 rounded ml-3  p-2  hover:bg-purple-300 hover:font-bold"
+            >
+              Cancel
             </button>
           </form>
         ) : (
@@ -192,6 +239,13 @@ export default function LandingPage() {
             >
               {submitCodeClicked ? "Submitting..." : "Submit"}
             </button>
+            <button
+              // type="submit"
+              onClick={cancelled}
+              className="mt-5 bg-red-400 rounded ml-3  p-2  hover:bg-purple-300 hover:font-bold"
+            >
+              Cancel
+            </button>
           </form>
         ) : (
           !uploadClicked && (
@@ -206,18 +260,21 @@ export default function LandingPage() {
         {(uploadClicked || receiveClicked) &&
           !uploadClicked &&
           showDownloadButton && (
-            <div className="flex items-center justify-center">
-              <button
-                onClick={handleDown}
-                className="bg-green-300 w-28 mt-8 rounded hover:font-bold hover:bg-purple-200"
-              >
-                DOWNLOAD
-              </button>
-            </div>
+            <>
+              {" "}
+              <h1 className="mt-5"> {received && received.fileName}</h1>
+              <div className="flex items-center justify-center">
+                <button
+                  onClick={handleDown}
+                  className="bg-green-300 w-28 mt-8 rounded hover:font-bold hover:bg-purple-200"
+                >
+                  DOWNLOAD
+                </button>
+              </div>
+            </>
           )}
       </div>
 
-      <h1>{received && received.fileName}</h1>
       {/* <img className="w-20 h-20" src={received && received.fileName} alt="" />  */}
 
       <div className="rules terms grid text-white mt-12">
