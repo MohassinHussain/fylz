f28cc6f6f41acc3932e4068bd094afed7cee5dd1
unknown <mohassinhussain22@gmail.com>
2024-08-03 16:04:22 +0530

    uri cut from index

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res)=>{
 })
 
 
-mongoose.connect(process.env.MONGO_KEY).then(() => {
+mongoose.connect("mongodb+srv://userme:OhguQudhETIKckYQ@cluster0.hwpdi97.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
     console.log("Connected");
     app.listen(port, () => {
         console.log("Listening to port");
